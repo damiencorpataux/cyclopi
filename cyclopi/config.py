965a5e6f7229670ecc53e6aebceafc3115dd0e37
Damien Corpataux <d@mien.ch>
2024-12-04 15:52:57 +0100

    Update config.py

--- cyclopi/config.py
+++ cyclopi/config.py
@@ -1,31 +1,11 @@
 # Cyclopi config example.
 
 wifi = ({
-    'ssid': 'CS-REZ-2G',
-    'psk': 'CSCHOOL-PASS-2019**',
+    'ssid': 'SSID-1',
+    'psk': 'password-1',
     'timeout': 15
 }, {
-    'ssid': 'Bbox-Baronnet',
-    'psk': 'zGSGZFRF94vnsS2T4r',
-    'timeout': 15
-}, {
-    'ssid': 'FKit',
-    'psk': 'pipicaca',
-    'timeout': 15
-}, {
-    'ssid': 'Sunrise_Wi-Fi_78AF80',
-    'psk': 'complyfilingmanage',
-    'timeout': 15
-    # 'ip': None,
-    # 'netmask': None,
-    # 'gateway': None,
-    # 'dns': None
-}, {
-    'ssid': 'FIXME-NAT',
-    'psk': 'hs1337_FIXME',
-    'timeout': 15
-}, {
-    'ssid': 'Restaurant Orchidée - Ale 25',
-    'psk': 'phoetfondue',
+    'ssid': 'SSID-2',
+    'psk': 'password-2',
     'timeout': 15
 })
